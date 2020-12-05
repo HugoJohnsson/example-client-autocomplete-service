3272c808f9bbfae9184887cc4d93ad21e9f53001
Hugo Johnsson <hugo.johnsson@stickerapp.com>
2020-12-05 23:03:43 +0100

    autocomplete suggestions now show up under the search bar while you type

--- src/index.js
+++ src/index.js
@@ -3,28 +3,62 @@ import './index.css';
 const apiUrl = 'http://localhost:5000';
 const getMatchingPhrasesEndpoint = '/api/v1/match/' // + prefix
 
+let isAutoCompleteElementsDisplayed = false;
+
 // Elements
 const searchInput = document.querySelector('.search__input');
 const matchingPhrases = document.querySelector('.search__matching_phrases');
 
+searchInput.focus();
+
 // Event listeners
 searchInput.addEventListener('input', async (e) => {
-    toggleAutoCompleteElements();
-
     const value = e.target.value;
 
-    if (value.length > 1) {
+    if (value.length > 0) {
         const response = await fetch(apiUrl + getMatchingPhrasesEndpoint + value);
-        const data = await response.json();
+        const phrases = await response.json();
 
-        console.log(data);
+        if (phrases.length > 0) {
+            displayMatchingPhrases(phrases.slice(0, 6));
+        } else {
+            matchingPhrases.innerHTML = "";
+            hideAutoCompleteElements();
+        }
+    } else {
+        matchingPhrases.innerHTML = "";
+        hideAutoCompleteElements();
     }
 });
 
 // Functions
-const toggleAutoCompleteElements = () => {
-    searchInput.classList.toggle('search__input--autocompleted');
+const displayMatchingPhrases = (phrases) => {
+    // Remove current results
+    matchingPhrases.innerHTML = "";
 
-    matchingPhrases.classList.toggle('search__matching_phrases--show');
-    matchingPhrases.classList.toggle('search__matching_phrases--autocompleted');
+    showAutoCompleteElements();
+
+    phrases.forEach(phrase => {
+        let el = document.createElement('div');
+        el.classList.add('search__matching_phrases_item');
+        el.textContent = phrase;
+
+        matchingPhrases.append(el);
+    });
+}
+
+const showAutoCompleteElements = () => {
+    isAutoCompleteElementsDisplayed = true;
+    searchInput.classList.add('search__input--autocompleted');
+
+    matchingPhrases.classList.add('search__matching_phrases--show');
+    matchingPhrases.classList.add('search__matching_phrases--autocompleted');
+}
+
+const hideAutoCompleteElements = () => {
+    isAutoCompleteElementsDisplayed = false;
+    searchInput.classList.remove('search__input--autocompleted');
+
+    matchingPhrases.classList.remove('search__matching_phrases--show');
+    matchingPhrases.classList.remove('search__matching_phrases--autocompleted');
 }
